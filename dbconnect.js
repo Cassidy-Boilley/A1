f34a3ce594ee43310edf64e364529f10dff786df
Cassidy Boilley <cboilley@my.bcit.ca>
2023-05-01 02:39:16 -0700

    fixes

--- dbconnect.js
+++ dbconnect.js
@@ -1,10 +1,14 @@
+
+
+const mongoose = require('mongoose');
+const app = require('./app');
 require('dotenv').config();
 
-const mongodb_host = process.env.MONGODB_HOST;
-const mongodb_user = process.env.MONGODB_USER;
-const mongodb_password = process.env.MONGODB_PASSWORD;
-
-const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/test`;
-var database = new MongoClient(atlasURI, {useNewUrlParser: true, useUnifiedTopology: true});
-module.exports = {database};
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(`${process.env.MONGODB_CONNECTION_STRING}`);
+  console.log("connected to db");
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running on port 3000');
+  });
+}
